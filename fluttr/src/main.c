03f74ce3256137a208eef5bba419e24c735d351e
Neil J. Patel <njp@openedhand.com>
2007-04-27 15:18:02 +0000

    Initial import

---
+++ fluttr/src/main.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2007 Neil J. Patel
+ * Copyright (C) 2007 OpenedHand Ltd
+ *
+ * Author: Neil J. Patel  <njp@o-hand.com>
+ */
+
+#include <config.h>
+#include <clutter/clutter.h>
+
+int
+main (int argc, char **argv)
+{
+ 
+	g_print ("fluttr\n");	
+
+	return 0;
+}
+
