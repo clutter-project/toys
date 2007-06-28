2acfb3e4163fa7a6ce4570c8793a59ac7821156b
Tomas Frydrych <tf@openedhand.com>
2007-06-28 07:44:09 +0000

    generalize test-dominatrix rectangle creation algorithm

--- widgets/test-dominatrix.c
+++ widgets/test-dominatrix.c
@@ -4,6 +4,10 @@
 #include <stdlib.h>
 #include "clutter-dominatrix.h"
 
+#define RECT_SIZE 120
+#define DIMX 3
+#define DIMY 3
+
 struct on_event_data 
 {
   ClutterDominatrix ** dmx;
@@ -32,7 +36,7 @@ on_event (ClutterStage *stage,
 	if (actor != d->reset)
 	    return;
 
-	for (i = 0; i < 9; ++i)
+	for (i = 0; i < DIMX * DIMY; ++i)
 	  {
 	    clutter_dominatrix_restore (*dmx);
 	    dmx++;
@@ -44,43 +48,29 @@ on_event (ClutterStage *stage,
     }
 }
 
-
-#define RECT_SIZE 120
-
 int
 main (int argc, char *argv[])
 {
   ClutterActor      * stage;
   ClutterActor      * label;
-  ClutterActor      * rect[9];
-  ClutterDominatrix * dmx[9];
-  ClutterGravity      gvt[9];
+  ClutterActor      * rect[DIMX * DIMY];
+  ClutterDominatrix * dmx[DIMX * DIMY];
   struct on_event_data d;
   gint i,j;
 
-  ClutterColor        clr[9] = { {0xff,0xff,0xff,0xff},
-				 {0xff,0,0,0xff},
-				 {0xff,0xff,0,0xff},
-				 {0xff,0,0xff,0xff},
-				 {0,0xff,0xff,0xff},
-				 {0,0xff,0,0xff},
-				 {0x7f,0x7f,0x7f,0xff},
-				 {0xff,0x5a,0,0xff},
-				 {0,0,0xff,0xff}};
+  ClutterColor        clr[DIMX * DIMY] = { {0xff,0xff,0xff,0xff},
+ 					   {0xff,0,0,0xff},
+ 					   {0xff,0xff,0,0xff},
+ 					   {0xff,0,0xff,0xff},
+ 					   {0,0xff,0xff,0xff},
+ 					   {0,0xff,0,0xff},
+					   {0x7f,0x7f,0x7f,0xff},
+					   {0xff,0x5a,0,0xff},
+					   {0,0,0xff,0xff}};
 	  
   ClutterColor        stage_color = { 0x0, 0x0, 0x0, 0xff }, 
 	              white       = { 0xff, 0xff, 0xff, 0xff };
 
-  gvt[0] = CLUTTER_GRAVITY_NORTH_WEST;
-  gvt[1] = CLUTTER_GRAVITY_NORTH;
-  gvt[2] = CLUTTER_GRAVITY_NORTH_EAST;
-  gvt[3] = CLUTTER_GRAVITY_WEST;
-  gvt[4] = CLUTTER_GRAVITY_CENTER;
-  gvt[5] = CLUTTER_GRAVITY_EAST;
-  gvt[6] = CLUTTER_GRAVITY_SOUTH_WEST;
-  gvt[7] = CLUTTER_GRAVITY_SOUTH;
-  gvt[8] = CLUTTER_GRAVITY_SOUTH_EAST;
-  
   clutter_init (&argc, &argv);
 
   stage = clutter_stage_get_default ();
@@ -88,11 +78,12 @@ main (int argc, char *argv[])
   clutter_stage_set_color (CLUTTER_STAGE (stage), &stage_color);
   clutter_actor_set_size (stage, 360, 410);
 
-  for (i = 0; i < 3; ++i)
-    for (j = 0; j < 3; ++j)
+  for (i = 0; i < DIMY; ++i)
+    for (j = 0; j < DIMX; ++j)
       {
-	gint k = i*3 + j;
-	
+	gint k = i * DIMX + j;
+	ClutterGravity gravity = CLUTTER_GRAVITY_CENTER;
+
 	rect[k] = clutter_rectangle_new ();
 	clutter_actor_set_position (rect[k],
 				    j*RECT_SIZE, 50 + i*RECT_SIZE);
@@ -101,7 +92,20 @@ main (int argc, char *argv[])
   	clutter_actor_show (rect[k]);
 	clutter_container_add (CLUTTER_CONTAINER (stage), rect[k], NULL);
 
-	dmx[k] = clutter_dominatrix_new_with_gravity (rect[k], gvt[k]);
+	if (i == 0 && j == 0)
+	    gravity = CLUTTER_GRAVITY_NORTH_WEST;
+	else if (i == DIMY - 1 && j == DIMX - 1)
+	    gravity = CLUTTER_GRAVITY_SOUTH_EAST;
+	else if (i == 0)
+	    gravity = CLUTTER_GRAVITY_NORTH;
+	else if (i == DIMY - 1)
+	    gravity = CLUTTER_GRAVITY_SOUTH;
+	else if (j == 0)
+	    gravity = CLUTTER_GRAVITY_WEST;
+	else if (j == DIMX - 1)
+	    gravity = CLUTTER_GRAVITY_EAST;
+	
+	dmx[k] = clutter_dominatrix_new_with_gravity (rect[k], gravity);
       }
   
   label = clutter_label_new_with_text ("Mono 8pt",
