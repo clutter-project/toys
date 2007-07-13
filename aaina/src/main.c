5b9ca340ce89d7bbfac6987a7baa3495cd195560
Neil J. Patel <njp@openedhand.com>
2007-07-13 17:12:14 +0000

    2007-07-13  Neil J. Patel  <njp@o-hand.com>
    	* src/main.c: (im_spinning_around), (main), (on_key_release_event),
    	(spin_me):
    	Added spinning of the stage via arrow keys,
    	Added a 1 minute timeout for spinning (needs work);

--- aaina/src/main.c
+++ aaina/src/main.c
@@ -76,7 +76,17 @@ static void on_key_release_event (ClutterStage *stage,
                                   ClutterEvent *event,
                                   gpointer      null);
 
+static void spin_me (ClutterBehaviour *behave, 
+                     guint32           alpha_value,
+                     gpointer          null);
 
+static gboolean
+im_spinning_around (ClutterTimeline *timeline)
+{
+  clutter_timeline_start (timeline);
+  return TRUE;
+}
+  
 int 
 main (int argc, char **argv)
 {
@@ -84,6 +94,9 @@ main (int argc, char **argv)
   AainaSource *source;
   ClutterActor *stage;
   AainaSlideShow *show;
+  ClutterTimeline *timeline;
+  ClutterAlpha *alpha;
+  ClutterBehaviour *behave;
   ClutterColor black = { 0x00, 0x00, 0x00, 0xff };
   GError *error = NULL;
 
@@ -138,20 +151,22 @@ main (int argc, char **argv)
   /*clutter_actor_set_scale (stage, 0.25, 0.25);*/
 
   g_signal_connect (G_OBJECT (stage), "key-release-event",
-                    G_CALLBACK (on_key_release_event), NULL);
+                    G_CALLBACK (on_key_release_event), (gpointer)stage);
 
-  /*
+  
   timeline = clutter_timeline_new (60, 30);
-  alpha = clutter_alpha_new_full (priv->timeline,
+  alpha = clutter_alpha_new_full (timeline,
                                   alpha_sine_inc_func,
                                   NULL, NULL);
   behave = aaina_behave_new (alpha, 
                              (AainaBehaveAlphaFunc)spin_me,
                              (gpointer)stage);
-  */
+  
   clutter_actor_rotate_y (stage, 0, 
                           CLUTTER_STAGE_WIDTH ()/2,
                           CLUTTER_STAGE_HEIGHT ());
+
+  g_timeout_add (10000, (GSourceFunc)im_spinning_around, timeline);
   clutter_main ();
 
   return EXIT_SUCCESS;
@@ -162,12 +177,46 @@ on_key_release_event (ClutterStage *stage,
                       ClutterEvent *event,
                       gpointer      null)
 {
+  static gint i = 0;
+  
   switch (clutter_key_event_symbol ((ClutterKeyEvent*)event))
   {
     case CLUTTER_Escape:
       clutter_main_quit ();
       break;
+    case CLUTTER_Left:
+       i--;
+       if (i == 0)
+         i = 359;
+       clutter_actor_rotate_y (CLUTTER_ACTOR (stage), i, 
+                               CLUTTER_STAGE_WIDTH ()/2,
+                               CLUTTER_STAGE_HEIGHT ());
+       break;
+    case CLUTTER_Right:
+       i++;
+       if (i == 360)
+         i = 0;
+       clutter_actor_rotate_y (CLUTTER_ACTOR (stage), i, 
+                               CLUTTER_STAGE_WIDTH ()/2,
+                               CLUTTER_STAGE_HEIGHT ());
+       break;
+
     default:
       break;
   }
 } 
+
+
+static void
+spin_me (ClutterBehaviour *behave, 
+         guint32           alpha_value,
+         gpointer          null)
+{
+ gfloat factor = (gfloat)alpha_value / CLUTTER_ALPHA_MAX_ALPHA;
+ 
+ clutter_actor_rotate_y (clutter_stage_get_default (),
+                         factor * 360,
+                         CLUTTER_STAGE_WIDTH ()/2,
+                         CLUTTER_STAGE_HEIGHT ());
+
+}
