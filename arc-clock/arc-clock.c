ba89fa456860dde07d9d4b03f7b29d96191d9166
Emmanuele Bassi <ebassi@linux.intel.com>
2010-07-02 16:06:31 +0100

    arc-clock: Show the date as well as the time

--- arc-clock/arc-clock.c
+++ arc-clock/arc-clock.c
@@ -6,31 +6,39 @@
 #include <clutter/clutter.h>
 #include <clutter/x11/clutter-x11.h>
 
-#define HAND_WIDTH      32
-
-#define N_HANDS         3
+#define HAND_WIDTH      24
 
 enum
 {
-  HOURS,
+  PM,
+
+  SECONDS,
   MINUTES,
-  SECONDS
+  HOURS,
+
+  DAY,
+  MONTH,
+
+  N_HANDS
 };
 
 static double slices[N_HANDS] = { 0.0, };
 
 static ClutterActor *hands[N_HANDS] = { NULL, };
 
-static ClutterBehaviour *behaviours[N_HANDS] = { NULL, };
-
 static const char *colors[N_HANDS] = {
+  "#4e9a06",
+
   "#edd400",
   "#ce5c00",
-  "#cc0000"
+  "#cc0000",
+
+  "#3465a4",
+  "#75507b"
 };
 
+static gboolean hide_date = FALSE;
 static gboolean hide_seconds = FALSE;
-static gboolean enable_animations = FALSE;
 
 static void
 hand_paint (ClutterActor *hand)
@@ -93,15 +101,12 @@ update_slices (gpointer data G_GNUC_UNUSED)
 
   slices[SECONDS] = now_tm->tm_sec * G_PI / 30;
   slices[MINUTES] = now_tm->tm_min * G_PI / 30;
-  slices[HOURS]   = (now_tm->tm_hour > 12 ? now_tm->tm_hour - 12
-                                          : now_tm->tm_hour) * G_PI / 6;
+  slices[HOURS]   = (now_tm->tm_hour >= 12 ? now_tm->tm_hour - 12
+                                           : now_tm->tm_hour) * G_PI / 6;
+  slices[DAY]     = now_tm->tm_mday * G_PI / 15;
+  slices[MONTH]   = (now_tm->tm_mon + 1) * G_PI / 6;
 
-  g_print ("%.3f hrs :: %.3f min :: %.3f sec\r",
-           slices[HOURS],
-           slices[MINUTES],
-           slices[SECONDS]);
-
-  for (int i = 0; i < N_HANDS; i++)
+  for (int i = SECONDS; i < N_HANDS; i++)
     clutter_actor_queue_redraw (hands[i]);
 
   return TRUE;
@@ -115,10 +120,10 @@ static GOptionEntry entries[] = {
     "Hide the seconds hand", NULL
   },
   {
-    "enable-animations", 'a',
+    "hide-date", 'd',
     0,
-    G_OPTION_ARG_NONE, &enable_animations,
-    "Animate the arcs", NULL
+    G_OPTION_ARG_NONE, &hide_date,
+    "Hide the date hands", NULL
   },
   { NULL }
 };
@@ -143,77 +148,40 @@ main (int argc, char *argv[])
   clutter_stage_set_title (CLUTTER_STAGE (stage), "Arc Clock");
   clutter_stage_set_user_resizable (CLUTTER_STAGE (stage), TRUE);
   clutter_stage_set_use_alpha (CLUTTER_STAGE (stage), TRUE);
-  clutter_actor_set_size (stage, 512, 512);
   clutter_actor_set_opacity (stage, 0);
 
   g_signal_connect (stage, "paint", G_CALLBACK (on_stage_pre_paint), NULL);
   g_signal_connect_after (stage, "paint", G_CALLBACK (on_stage_post_paint), NULL);
   g_signal_connect (stage, "destroy", G_CALLBACK (clutter_main_quit), NULL);
 
-  ClutterColor color;
-
-  clutter_color_from_string (&color, colors[HOURS]);
-  hands[HOURS] = clutter_rectangle_new_with_color (&color);
-  clutter_actor_set_size (hands[HOURS], 384, 384);
-  clutter_actor_add_constraint (hands[HOURS], clutter_align_constraint_new (stage, CLUTTER_ALIGN_X_AXIS, 0.5));
-  clutter_actor_add_constraint (hands[HOURS], clutter_align_constraint_new (stage, CLUTTER_ALIGN_Y_AXIS, 0.5));
-  g_signal_connect (hands[HOURS], "paint", G_CALLBACK (hand_paint), NULL);
-  g_object_set_data (G_OBJECT (hands[HOURS]), "hand-id", GUINT_TO_POINTER (HOURS));
-
-  clutter_color_from_string (&color, colors[MINUTES]);
-  hands[MINUTES] = clutter_rectangle_new_with_color (&color);
-  clutter_actor_set_size (hands[MINUTES], 256, 256);
-  clutter_actor_add_constraint (hands[MINUTES], clutter_align_constraint_new (stage, CLUTTER_ALIGN_X_AXIS, 0.5));
-  clutter_actor_add_constraint (hands[MINUTES], clutter_align_constraint_new (stage, CLUTTER_ALIGN_Y_AXIS, 0.5));
-  g_signal_connect (hands[MINUTES], "paint", G_CALLBACK (hand_paint), NULL);
-  g_object_set_data (G_OBJECT (hands[MINUTES]), "hand-id", GUINT_TO_POINTER (MINUTES));
-
-  clutter_color_from_string (&color, colors[SECONDS]);
-  hands[SECONDS] = clutter_rectangle_new_with_color (&color);
-  clutter_actor_set_size (hands[SECONDS], 128, 128);
-  clutter_actor_add_constraint (hands[SECONDS], clutter_align_constraint_new (stage, CLUTTER_ALIGN_X_AXIS, 0.5));
-  clutter_actor_add_constraint (hands[SECONDS], clutter_align_constraint_new (stage, CLUTTER_ALIGN_Y_AXIS, 0.5));
-  g_signal_connect (hands[SECONDS], "paint", G_CALLBACK (hand_paint), NULL);
-  g_object_set_data (G_OBJECT (hands[SECONDS]), "hand-id", GUINT_TO_POINTER (SECONDS));
-
-  ClutterTimeline *timeline = clutter_timeline_new (5000);
-  clutter_timeline_set_loop (timeline, TRUE);
-
-  if (enable_animations)
+  for (int i = SECONDS; i < N_HANDS; i++)
     {
-      ClutterAlpha *alpha = clutter_alpha_new_full (timeline, CLUTTER_LINEAR);
-      g_object_unref (timeline);
+      ClutterColor color;
 
-      behaviours[HOURS] = clutter_behaviour_rotate_new (alpha, CLUTTER_Y_AXIS,
-                                                        CLUTTER_ROTATE_CW,
-                                                        0.0, 0.0);
-      clutter_behaviour_rotate_set_center (CLUTTER_BEHAVIOUR_ROTATE (behaviours[HOURS]), 196, 0, 0);
-      clutter_behaviour_apply (behaviours[HOURS], hands[HOURS]);
-
-      behaviours[MINUTES] = clutter_behaviour_rotate_new (alpha, CLUTTER_Y_AXIS,
-                                                          CLUTTER_ROTATE_CCW,
-                                                          0.0, 0.0);
-      clutter_behaviour_rotate_set_center (CLUTTER_BEHAVIOUR_ROTATE (behaviours[MINUTES]), 128, 0, 0);
-      clutter_behaviour_apply (behaviours[MINUTES], hands[MINUTES]);
+      clutter_color_from_string (&color, colors[i]);
+      hands[i] = clutter_rectangle_new_with_color (&color);
+      clutter_actor_set_size (hands[i], (HAND_WIDTH * 3.0) * i, (HAND_WIDTH * 3.0) * i);
+      clutter_actor_add_constraint (hands[i], clutter_align_constraint_new (stage, CLUTTER_ALIGN_X_AXIS, 0.5));
+      clutter_actor_add_constraint (hands[i], clutter_align_constraint_new (stage, CLUTTER_ALIGN_Y_AXIS, 0.5));
+      g_signal_connect (hands[i], "paint", G_CALLBACK (hand_paint), NULL);
+      g_object_set_data (G_OBJECT (hands[i]), "hand-id", GUINT_TO_POINTER (i));
+      clutter_container_add_actor (CLUTTER_CONTAINER (stage), hands[i]);
     }
-
-  clutter_container_add (CLUTTER_CONTAINER (stage),
-                         hands[HOURS],
-                         hands[MINUTES],
-                         hands[SECONDS],
-                         NULL);
 
   if (hide_seconds)
     clutter_actor_hide (hands[SECONDS]);
+
+  if (hide_date)
+    {
+      clutter_actor_hide (hands[DAY]);
+      clutter_actor_hide (hands[MONTH]);
+    }
 
   g_timeout_add_seconds ((hide_seconds ? 60 : 1), update_slices, NULL);
 
   update_slices (NULL);
 
   clutter_actor_show (stage);
-
-  if (enable_animations)
-    clutter_timeline_start (timeline);
 
   clutter_main ();
 
