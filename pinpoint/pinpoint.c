8dc63b8092dc360549e03767d28b7dd7796b166e
Damien Lespiau <damien.lespiau@intel.com>
2010-07-21 00:34:04 +0100

    pinpoint: Relicense to LGPL v2.1+
    Because of Pinpoint using a function inspired by some Gdk code, we have
    to relicense it to LGPL v2+, say v2.1+.

--- pinpoint/pinpoint.c
+++ pinpoint/pinpoint.c
@@ -1,30 +1,24 @@
 /*
-Copyright (c) 2010, Intel Corporation
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-• Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-• Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-• Neither the name of Intel Corporation nor the names of its contributors may
-  be used to endorse or promote products derived from this software without
-  specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Poinpoint: A small-ish presentation tool
+ *
+ * Copyright (C) 2010 Intel Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option0 any later version.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Written by: Øyvind Kolås <pippin@linux.intel.com>
+ *             Damien Lespiau <damien.lespiau@intel.com>
+ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -725,6 +719,10 @@ cairo_renderer_init (PinPointRenderer *pp_renderer,
                                               NULL, _destroy_surface);
 }
 
+/* This function is adapted from Gtk's gdk_cairo_set_source_pixbuf() you can
+ * find in gdk/gdkcairo.c.
+ * Copyright (C) Red Had, Inc.
+ * LGPLv2+ */
 static cairo_surface_t *
 _cairo_new_surface_from_pixbuf (const GdkPixbuf *pixbuf)
 {
